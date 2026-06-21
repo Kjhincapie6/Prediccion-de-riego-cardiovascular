6beb236b84a11b59d32d280c446747bcce902de2
Kely Jhojana Hincapie Zapata <kely.hincapie@uniminuto.edu.co>
2026-06-20 19:26:26 -0500

    Change Authorization header from Token to Bearer

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ DEPLOYMENT_ID = os.getenv("DATAROBOT_DEPLOYMENT_ID")
 HOST = os.getenv("DATAROBOT_HOST")
 
 headers = {
-    "Authorization": f"Token {API_KEY}",  # usa Token en lugar de Bearer
+  "Authorization": f"Bearer {API_KEY}",  # usa Token en lugar de Bearer
     "Content-Type": "application/json"
 }
 
