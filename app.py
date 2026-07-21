0b70e0ef28541f2b71e4f542676b2425e0107cbf
Kely Jhojana Hincapie Zapata <kely.hincapie@uniminuto.edu.co>
2026-07-21 13:19:47 -0500

    Import train_test_split and fix main call indentation

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sys
 import os
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, roc_auc_score, classification_report
 import joblib
